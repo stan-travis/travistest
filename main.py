00d140e87926cc3f1eff248b74a508588eb1c76b
stan-travis <79876155+stan-travis@users.noreply.github.com>
2021-04-08 14:28:17 +0200

    Update main.py
    Next update

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello World! Hello TravisCI!!!')
-    print_hi('Hello! Testing Travis - want to trigger by commit')
+    print_hi('Hello! Testing Travis - lets see now')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
